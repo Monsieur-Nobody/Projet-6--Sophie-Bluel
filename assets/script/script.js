e365bd95d8acde810592aa8cd043b1fd01b5794f
Your Name <121236744+Monsieur-Nobody@users.noreply.github.com>
2024-06-03 18:19:22 +0200

    03/06

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,106 @@
-let gallery = document.getElementById(gallery)
+// Logique de création du code Javascript 
+
+/* 
+1 - Récupérer les images présentes dans la db
+2 - Afficher ses images sur le site via Js
+3 - Récupérer les catégories présentes dans la db
+4 - Afficher les quatres boutons "filtres" 
+5 - Rendre fonctionnels ces quatres boutons
+
+*/
+// Variable correspondants a la div HTML
+const $gallery = document.querySelector(".gallery");
+//Variable contenant la div filtres
+const $filtreContainer = document.querySelector(".filtres");
+// Variable contenant le tableau avec les images
+let $images = [];
+// Variable contenant le tableau des categories
+let $categories = [];
+// Variable contenant la route de l'API 
+const $routeApi = "http://localhost:5678/api/";
+
+const recupererImage = () => {
+    //On recupére les images via la route works avec une requete HTTP
+    fetch(`${$routeApi}works`)
+    //then nous sert a "renvoyer une promesse" et nous evite d'utiliser await, et nous assure que l'action est bien le temps de s'effectuer 
+    .then((rep) => rep.json())
+    // le second then traite les données qui viennent d'etre créer dans le json et les affiche via le console.log
+    .then((data) => {
+        // console.log(data)
+        // Maintenant qu'on a vérifié qu'on recupere bien les informations via le console.log on les attribue a la variable $images
+        $images = data;
+        afficherImages(0);
+    })
+    // catch nous permet d'attraper toutes les erreurs possibles et de les afficher via console.log 
+    .catch((e) => console.log(e))
+};
+
+const recupererCategories = () => {
+     //On recupére les categories via la route categories avec une requete HTTP
+     fetch(`${$routeApi}categories`)
+     //then nous sert a "renvoyer une promesse" et nous evite d'utiliser await, et nous assure que l'action est bien le temps de s'effectuer 
+     .then((rep) => rep.json())
+     // le second then traite les données qui viennent d'etre créer dans le json et les affiche via le console.log
+     .then((data) => {
+         //  console.log(data)
+         // Maintenant qu'on a vérifié qu'on recupere bien les informations via le console.log on les attribue a la variable $images
+         $categories = data;
+         afficherCategories();
+     })
+     // catch nous permet d'attraper toutes les erreurs possibles et de les afficher via console.log 
+     .catch((e) => console.log(e))
+}
+
+const afficherImages = (idCat) => {
+
+    $gallery.innerHTML = "";
+
+    $images.forEach((i) => {
+        // console.log(i)
+        console.log(idCat == i.categoryId)
+        if (idCat == 0 ){
+            $gallery.innerHTML += `  <figure>
+                                <img src='${i.imageUrl}' alt='${i.title}'>
+                                <figcaption>${i.title}</figcaption>
+                                </figure>   `
+                            
+        } else if ( idCat == i.categoryId) {
+            $gallery.innerHTML += `  <figure>
+                                <img src='${i.imageUrl}' alt='${i.title}'>
+                                <figcaption>${i.title}</figcaption>
+                                </figure>   `
+        }
+       
+    })
+
+
+}
+
+const afficherCategories = () => {
+
+
+    const $btn1 = document.createElement('button');
+                    $btn1.innerHTML= `Tous`;
+                    $filtreContainer.appendChild($btn1)
+                    $btn1.addEventListener('click' , () => {
+                        afficherImages(0);
+                    })
+        
+    $categories.forEach((cat) => {
+        // console.log(c)
+
+
+        const $btn = document.createElement('button');
+                    $btn.innerHTML= `${cat.name}`;
+                    $btn.addEventListener('click', () => {
+                        afficherImages(cat.id)
+                    })
+                    $filtreContainer.appendChild($btn)
+        
+    })
+
+
+}
+
+recupererImage();
+recupererCategories();
